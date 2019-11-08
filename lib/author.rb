f9482f63b50313d28e066bbbb1be3838d7e3cfc7
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-08 00:45:18 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+  
+  attr_accessor :name, :post
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def posts
+    Post.all.select do |post|
+      post.author == self
+    end
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(post_title)
+    not_title = Post.new(post_title)
+    add_post(not_title)
+  end
+  
+  
+end
